3d5e9799a09c96ed21edd30cd00a6432175031ea
swapnil.masurekar <swapnil.masurekar@quantiphi.com>
2020-09-02 13:31:58 +0000

    eval count

--- efficientdet/evaluate.sh
+++ efficientdet/evaluate.sh
@@ -3,4 +3,4 @@ python main.py --mode=eval  \
     --model_dir=gs://tfrecords_and_logs/experiments/img_clf_approach/logs/efficientdet_5_wbcs/  \
     --validation_file_pattern=gs://tfrecords_and_logs/experiments/img_clf_approach/tfrecord_obj_5_wbcs/valid*.tfrecord  \
     --testdev_dir='../validation_results/' \
-    --eval_samples=20288
+    --eval_samples=534
