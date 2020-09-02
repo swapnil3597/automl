a6c0f69510b43647e50c7d8730a6c6b6e0c373f2
swapnil.masurekar <swapnil.masurekar>
2020-09-02 05:18:20 +0000

    mv testdata

--- efficientdet/inference.sh
+++ efficientdet/inference.sh
@@ -1,4 +1,4 @@
 python model_inspect.py --runmode=saved_model_infer \
   --model_name=efficientdet-d0  \
   --saved_model_dir=gs://tfrecords_and_logs/experiments/img_clf_approach/logs/efficientdet/export/  \
-  --input_image=testdata/Task_13_2020142361_1_001.png --output_image_dir=results/
+  --input_image=../testdata/Task_13_2020142361_1_001.png --output_image_dir=results/
