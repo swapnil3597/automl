ec6da726ad24e7e352674d32dd8b3bc92bff0d4b
swapnil.masurekar <swapnil.masurekar@quantiphi.com>
2020-09-02 13:21:10 +0000

    eval sh

---
+++ efficientdet/evaluate.sh
@@ -0,0 +1,6 @@
+python main.py --mode=eval  \
+    --model_name=efficientdet-d0 \
+    --model_dir=gs://tfrecords_and_logs/experiments/img_clf_approach/logs/efficientdet_5_wbcs/  \
+    --validation_file_pattern=gs://tfrecords_and_logs/experiments/img_clf_approach/tfrecord_obj_5_wbcs/valid*.tfrecord  \
+    --testdev_dir='../validation_results/' \
+    --eval_samples=20288
