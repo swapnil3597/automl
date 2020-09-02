75587c5e0d05673f14ecba8556b1e0e66c243dca
swapnil.masurekar <swapnil.masurekar@quantiphi.com>
2020-09-02 17:04:01 +0000

    infer

--- efficientdet/inference.sh
+++ efficientdet/inference.sh
@@ -1,4 +1,4 @@
 python model_inspect.py --runmode=saved_model_infer \
   --model_name=efficientdet-d0  \
-  --saved_model_dir=gs://tfrecords_and_logs/experiments/img_clf_approach/logs/efficientdetd0_neutro_lymph_plate_2ep/export/  \
-  --input_image=../test_data/Task_13_2020142361_1_001.png --output_image_dir=results/
+  --saved_model_dir=gs://tfrecords_and_logs/experiments/img_clf_approach/logs/efficientdet_5_wbcs/export/  \
+  --input_image=../test_data/*.png --output_image_dir=results/
