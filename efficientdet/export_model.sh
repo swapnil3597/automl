c43a97201b52f682558bd740aaf45140201f7824
swapnil.masurekar <swapnil.masurekar@quantiphi.com>
2020-09-02 17:01:07 +0000

    export

--- efficientdet/export_model.sh
+++ efficientdet/export_model.sh
@@ -1,4 +1,6 @@
+# TODO: CHECK THE HPARAMS CONFIG
+
 python model_inspect.py --runmode=saved_model --model_name=efficientdet-d0 \
-  --ckpt_path=gs://tfrecords_and_logs/experiments/img_clf_approach/logs/efficientdet/ \
-  --saved_model_dir=gs://tfrecords_and_logs/experiments/img_clf_approach/logs/efficientdet/export/ \
-  --tensorrt=FP32  --tflite_path=gs://tfrecords_and_logs/experiments/img_clf_approach/logs/efficientdet/tflite/
+  --ckpt_path=gs://tfrecords_and_logs/experiments/img_clf_approach/logs/efficientdet_5_wbcs/ \
+  --saved_model_dir=gs://tfrecords_and_logs/experiments/img_clf_approach/logs/efficientdet_5_wbcs/export/ \
+  --tensorrt=FP32  --tflite_path=gs://tfrecords_and_logs/experiments/img_clf_approach/logs/efficientdet_5_wbcs/tflite/
